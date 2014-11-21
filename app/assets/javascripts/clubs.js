4540c6f28abe5e6094b3d3bff56d9406603e67bd
Bookis Smuin <bookis.smuin@gmail.com>
2014-11-21 10:38:26 -0800

    finish js

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -4,7 +4,10 @@ $(function() {
     // select the (grand)parent div of `this` with a class of club
     // add a class of `chosen` to that div
     var $club = $(this).parents(".club");
+    var $form = $(this).parents("form");
     $club.addClass("chosen");
-    $.ajax("/");
+    $.ajax($form.attr("action"), {
+      type: "POST"
+    });
   });
 });
--- app/models/player_club.rb
+++ app/models/player_club.rb
@@ -1,2 +1,4 @@
 class PlayerClub < ActiveRecord::Base
+  belongs_to :club
+  belongs_to :player
 end
