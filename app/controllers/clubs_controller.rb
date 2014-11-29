6e5e7a4b98a8ff9a771e90f22ae07f09f119f5bd
Bookis Smuin <bookis.smuin@gmail.com>
2014-11-29 11:16:57 -0800

    add create action

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -2,4 +2,11 @@ class ClubsController < ApplicationController
   def index
     @clubs = Club.all
   end
+
+  def create
+    @club = Club.new(params.require(:club).permit(:name, :category, :handed))
+    if @club.save
+      redirect_to root_path
+    end
+  end
 end
