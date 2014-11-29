e7918dc38a4cf6975b4515b4f5d7c61d3a80a0c5
Bookis Smuin <bookis.smuin@gmail.com>
2014-11-29 09:52:08 -0800

    update js

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -5,9 +5,14 @@ $(function() {
     // add a class of `chosen` to that div
     var $club = $(this).parents(".club");
     var $form = $(this).parents("form");
-    $club.addClass("chosen");
     $.ajax($form.attr("action"), {
-      type: "POST"
+      type: "POST",
+      success: function() {
+        $club.addClass("chosen");
+      },
+      error: function() {
+        alert("ERROR!!");
+      }
     });
   });
 });
