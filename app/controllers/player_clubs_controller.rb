d105c04dccdef3e34b442efade9138141bb3eef9
Bookis Smuin <bookis.smuin@gmail.com>
2014-12-01 11:04:47 -0800

    remove resque

--- app/controllers/player_clubs_controller.rb
+++ app/controllers/player_clubs_controller.rb
@@ -2,7 +2,6 @@ class PlayerClubsController < ApplicationController
   def create
     @club = Club.find(params[:club_id])
     PlayerClub.create(club_id: @club.id, player_id: current_user.id)
-    Resque.enqueue(EmailJob, @club.id)
     # That line ^ does the same as:
     # current_user.clubs << @club
     redirect_to root_path
