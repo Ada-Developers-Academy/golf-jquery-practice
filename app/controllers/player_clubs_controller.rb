c5807426aacc29cc59ff13d1880832591f5f2e7f
Bookis Smuin <bookis.smuin@gmail.com>
2014-12-02 11:05:14 -0800

    add resque

--- app/controllers/player_clubs_controller.rb
+++ app/controllers/player_clubs_controller.rb
@@ -2,7 +2,8 @@ class PlayerClubsController < ApplicationController
   def create
     @club = Club.find(params[:club_id])
     if PlayerClub.create(club_id: @club.id, player_id: current_user.id)
-      ClubMailer.join_us(@club.id).deliver
+      # ClubMailer.join_us(@club.id).deliver
+      Resque.enqueue(EmailJob, @club.id)
       redirect_to root_path
     else
       #
--- app/jobs/email_job.rb
+++ app/jobs/email_job.rb
@@ -1,5 +1,6 @@
 class EmailJob
   @queue = :email
+
   def self.perform(club_id)
     ClubMailer.join_us(club_id).deliver
   end
