358acd1da9c2888f67f3b009713b4e7326c0850a
Kari Bancroft <kariabancroft@gmail.com>
2015-08-25 18:39:08 -0700

    Update per C3 requirements

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -2,9 +2,6 @@ $(function() {
   $(".choose").click(function(event) {
     event.preventDefault();
     // select the (grand)parent div of `this` with a class of club
-    // add a class of `chosen` to that div
-    var $club = $(this).parents(".club");
-    $club.addClass("chosen");
-    $.ajax("/");
+    // add a class of `chosen` to that div    
   });
 });
